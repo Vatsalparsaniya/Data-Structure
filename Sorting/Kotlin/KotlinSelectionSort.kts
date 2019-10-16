47a5ce3f1ec13c09f5c90c15c26f750c3210c091
Andreas Oen <andreas.oentoro@bukalapak.com>
2019-10-16 08:45:14 +0700

    Add kotlin selection sort

---
+++ Sorting/Kotlin/KotlinSelectionSort.kts
@@ -0,0 +1,28 @@
+fun selectionSort(arr: Array<Int>): Array<Int> {
+    val dataSize = arr.size
+    var key = 0
+    var prev = 0
+
+    for (current in 1 until dataSize) {
+        key = arr[current]
+        prev = current - 1
+        while (prev >= 0 && arr[prev] > key) {
+            arr[prev + 1] = arr[prev]
+            prev -= 1
+        }
+
+        arr[prev + 1] = key
+        arr.forEach {
+            print("$it ")
+        }
+        print("\n")
+    }
+    return arr
+}
+
+fun main() {
+    val testCase = arrayOf(5, 3, 4, 2, 1, 10, 9)
+    selectionSort(testCase).forEach {
+        print("$it ")
+    }
+}
