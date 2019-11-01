344f73f8dc65addb7cb6028d84d3e88ff54ae3d1
Chuck Underwood <chillieguy@live.com>
2019-10-31 19:53:32 -0700

    Add Fizz Buzz in Javascript

---
+++ FizzBuzz_Javascript/fizzbuzz.js
@@ -0,0 +1,13 @@
+function fizzBuzz(){
+	for(var i=1;i<=100;i++){
+		if(i%5 === 0 && i%3 === 0){
+			print('FizzBuzz');
+		} else if(i%3 === 0){
+			print('Fizz');
+		} else if(i%5 === 0){
+			print('Buzz');
+		} else {
+			print(i);
+		}
+	}
+}
