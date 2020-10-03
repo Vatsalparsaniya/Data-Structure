d1c0a2ffde3cfe192b97271edb09608d7c7a1c39
Yves Courteau <kourto05@hotmail.com>
2020-10-02 20:24:10 -0400

    Created a function with the algorithm
    I put the code inside a function since I did it first in the main program haha..

--- Sorting/shell_sort.c
+++ Sorting/shell_sort.c
@@ -1,39 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void main(void){
-    int i, inner, outer, temp, h;
-    int a[5] = {4,3,2,1,0};
-    size_t len = sizeof(a) / sizeof(int);
-
-    for(i=0;i<len;i++){
-        printf("%d ", a[i]);
-    }
-    printf("\n");
-
+void shellsort(int *a, size_t len){
+    int inner, outer, temp, h;
     h = 1;
 
-    while(h <= len/3)
-        h=h*3+1;
+    while(h <= len / 3)
+        h = h * 3 + 1;
     
-    while(h>0)
+    while(h > 0)
     {
-        for(outer=h; outer<len; outer++)
+        for(outer = h; outer < len; outer++)
         {
-            temp =a[outer];
+            temp = a[outer];
             inner = outer;
 
             while(inner > h-1 && a[inner-h] >= temp)
             {
-                a[inner] =a[inner-h];
+                a[inner] = a[inner - h];
                 inner -= h;
             }
             a[inner] = temp;
         }
-        h=(h-1)/3;
+        h = (h - 1) / 3;
     }
+}
 
-    for(i=0;i<len;i++){
+void main(void){
+    int i;
+    int a[5] = {4,3,2,1,0};
+    size_t len = sizeof(a) / sizeof(int);
+
+    for(i = 0; i < len; i++){
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    shellsort(a, len);
+    
+
+    for(i = 0; i < len; i++){
         printf("%d ", a[i]);
     }
     printf("\n");
